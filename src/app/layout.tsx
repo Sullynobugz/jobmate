ce345ee35968bf0af1e2e95fb98b8f9741e4f255
sullynobugz <bastian.siedenburg@gmx.de>
2026-06-11 23:35:05 +0200

    feat: add posthog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { PHProvider } from './providers'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -20,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de" className={`h-full ${inter.variable}`}>
-      <body className="min-h-full flex flex-col antialiased">{children}</body>
+      <body className="min-h-full flex flex-col antialiased"><PHProvider>{children}</PHProvider></body>
     </html>
   )
 }
